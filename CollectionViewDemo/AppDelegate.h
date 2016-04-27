bfd089a25338be2edad1fc132e16b7e8a775c9fa
吴鹏 <457760698@qq.com>
2016-04-27 16:10:09 +0800

    Initial Commit

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by gaiaworks on 16/4/27.
+//  Copyright © 2016年 gaiaworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
